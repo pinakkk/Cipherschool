eb5123020d70607a6a5dcfaad1dc184f37ef2c56
pinak <pinakkundu1080@gmail.com>
2024-08-29 20:38:42 +0530

    added day3 sol

---
+++ Day3/ques2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    unsigned long long factorial = 1;
+
+    cout << "Enter a number: ";
+    cin >> N;
+
+    for (int i = 1; i <= N; ++i) {
+        factorial *= i;
+    }
+
+    cout << "The factorial of " << N << " is: " << factorial << endl;
+
+    return 0;
+}
